a03bcebdd09b8dc50bb406d0e865cbb68d4137a6
LeeMinjae <LMJ0900@github.com>
2026-01-02 16:46:19 +0900

    [Feature] inputBox useForm 적용 (mutaion 연결전)

--- src/app/auth/login/_component/LoginForm/LoginForm.tsx
+++ src/app/auth/login/_component/LoginForm/LoginForm.tsx
@@ -1,5 +1,14 @@
 'use client';
 
+import { useForm } from 'react-hook-form';
+
+import { classValidatorResolver } from '@hookform/resolvers/class-validator';
+
+import ActionButton from '@/app/(main)/_component/ActionButton';
+import { LoginInputModel } from '@/app/auth/login/_component/LoginForm/model/LoginInputModel';
+
+import InputBox from '@/component/Input/InputBox';
+
 import { bindClassNames } from '@/util/BindClassName';
 
 import styles from './LoginForm.module.css';
@@ -9,9 +18,44 @@ const cx = bindClassNames(styles);
 type Props = {};
 
 export default function LoginForm({}: Props) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<LoginInputModel>({
+    mode: 'all',
+    resolver: classValidatorResolver(LoginInputModel),
+  });
+
+  const onSubmit = (data: LoginInputModel) => {
+    console.log('submit:', data);
+  };
+
   return (
     <div className={cx('root')}>
-      <div>component</div>
+      <div className={cx('title')}>Login</div>
+      <form className={cx('form')} onSubmit={handleSubmit(onSubmit)}>
+        <InputBox
+          inputRegisterReturn={register('email')}
+          label="Email"
+          inputType="email"
+          inputId="email"
+          placeholder="username@gmail.com"
+          currentInputValue={watch().email}
+          errorMessage={errors.email?.message}
+        />
+        <InputBox
+          inputRegisterReturn={register('password')}
+          label="Password"
+          inputType="password"
+          inputId="password"
+          placeholder="Password"
+          currentInputValue={watch().password}
+          errorMessage={errors.password?.message}
+        />
+      </form>
+      <ActionButton className={cx('submitBtn')} label="submit" onClick={handleSubmit(onSubmit)} />
     </div>
   );
 }
